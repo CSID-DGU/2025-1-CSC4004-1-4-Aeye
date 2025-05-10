2cb98f8f7e4c2aea275897b8ac0ea0bce4493948
Hyungeun Kim <174528308+v1340414@users.noreply.github.com>
2025-05-10 10:50:00 +0900

    [feat] #10 - add image preprocessing(upscaling & slicing)

--- backend/AI/ocr.py
+++ backend/AI/ocr.py
@@ -1,7 +1,10 @@
 from PIL import Image
+import cv2
+import numpy as np
 import io
 from google.cloud import vision
 from google.oauth2 import service_account
+import re
 
 credentials = service_account.Credentials.from_service_account_file('service-account-file.json')
 vision_client = vision.ImageAnnotatorClient(credentials=credentials)
@@ -21,3 +24,30 @@ def call_google_ocr_api(img_np):
         return annotations[0].description
     else:
         return ""
+    
+# 이미지 전처리(업스케일링)
+def preprocess_img(img_stream, upscale_factor):
+    img = Image.open(img_stream).convert('RGBA')
+    img_np = np.array(img)
+    height, width = img_np.shape[:2] 
+
+    img_upscaled = cv2.resize(img_np, (width * upscale_factor, height * upscale_factor), interpolation=cv2.INTER_CUBIC)
+    img_gray = cv2.cvtColor(img_upscaled, cv2.COLOR_BGR2GRAY)
+    height, width = img_gray.shape
+
+    slices = []
+    if height > 1000 * upscale_factor:
+        step = 500 * upscale_factor
+        overlap = 500
+        y = 0
+        while y < height:
+            y_end = min(y + step, height)
+            slice_img = img_gray[y:y_end, :]
+            slices.append(slice_img)
+            if y_end == height:
+                break
+            y = y + (step - overlap)
+    else:
+        slices.append(img_gray)
+
+    return slices
