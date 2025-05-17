0856f6c015022612455138274904a963a4064be4
Gill <83386076+Gill-source@users.noreply.github.com>
2025-05-17 17:22:39 +0900

    변수명 수정

--- backend/background.js
+++ backend/background.js
@@ -5,14 +5,12 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         
         // 서버로 전송할 데이터
         const data = {
-            name: productData.name || "상품명 없음",
-            price: productData.price || "가격 없음",
-            more_option: productData["more_option"] || "옵션 없음",
+            name: productData["name"]|| "상품명 없음",
+            price: productData["price"]|| "가격 없음",
             shipping_fee: productData["shipping_fee"] || "배송 정보 없음",
-            commentData: productData["commentData"] || "리뷰 없음",
-             worstCommentData: productData["worstCommentData"] || "부정 리뷰 없음",
+            comment_data: productData["comment_data"] || "리뷰 없음",
             average_grade: productData["average_grade"] || "평균 평점 없음", 
-            review_All:productData["review_All"]||"리뷰 정보 없음",
+            review_all:productData["review_all"]||"리뷰 정보 없음",
             review_length: productData["review_length"] || 0,
     };
 
@@ -49,24 +47,22 @@ async function summarize_2(data, sendResponse) {
                     return `[${index + 1}] 
 상품명: ${item.name || '이름 없음'} 
 가격: ${item.price || '없음'}
-옵션정보: ${item.more_option || '없음'}
 배송정보: ${item.shipping_fee || '없음'}
 평균만족도" ${item.average_grade || '없음'}
-리뷰정보: ${item.review_All || '없음'}
+리뷰정보: ${item.review_all || '없음'}
 리뷰개수: ${item.review_length || '없음'}
-대표리뷰: ${item.commentData || '없음'}`
+대표리뷰: ${item.comment_data || '없음'}`
 ;
                 }).join('\n');
             } else if (typeof resultData === "object" && resultData !== null) {
                 result = `
 상품명: ${resultData.name || '이름 없음'} 
 가격: ${resultData.price || '없음'}
-옵션정보: ${resultData.more_option || '없음'} 
 배송정보: ${resultData.shipping_fee || '없음'}
 평균만족도: ${resultData.average_grade || '없음'}
-리뷰정보: ${resultData.review_All||'없음'}
+리뷰정보: ${resultData.review_all||'없음'}
 리뷰개수: ${resultData.review_length || '없음'}
-대표리뷰: ${resultData.commentData || '없음'}`;
+대표리뷰: ${resultData.comment_data || '없음'}`;
             } else {
                 result = JSON.stringify(resultData, null, 2);
                 console.error("❌ Unexpected data format:", resultData);
